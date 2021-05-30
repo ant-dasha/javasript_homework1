8932b128943bb04400e5647b2f30d7d3b40dd8d2
ant-dasha <antonenko6571@gmail.com>
2021-05-30 17:37:27 +0300

    Add let month, let test, let d, lets

---
+++ script.js
@@ -0,0 +1,44 @@
+let month = 12;
+if (month = 1, 2, 12) {
+    console.log ('Зима');
+}
+else if (month >= 3) {
+    console.log ('Весна');
+}
+else if (month < 6) {
+    console.log('Весна')
+}
+else if (month >= 6) {
+    console.log ('Лето');
+}
+else if (month < 9) {
+    console.log ('Лето');
+}
+else if (month >= 9) {
+    console.log ('Осень');
+}
+else if (month < 12) {
+    console.log ('Зима');
+}
+
+
+/*Если переменная test равна true, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.*/
+ let test = true;
+ if (test = true) {
+     alert ('Верно');
+ }
+ else {
+     alert ('Неверно')
+ }
+  
+ /*Найдите количество секунд в сутках.*/
+let d = 24;
+let h = 60;
+let m = 60;
+ alert (d*h*m)
+
+ /*Пусть дана переменная r с радиусом круга. По соответствующей формуле найдите площадь круга и запишите ее в переменную s. Выведите содержимое этой переменной на экран.*/
+ let r = 10;
+ let s = 3.14*10^2;
+ alert ('s')
+
